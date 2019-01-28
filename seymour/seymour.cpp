0aacb0a5016914155c9186e0970c3f68234c3850
Leif Christiansen <leifchri@iu.edu>
2019-01-28 15:22:19 -0500

    updated code

--- seymour/seymour.cpp
+++ seymour/seymour.cpp
@@ -51,20 +51,22 @@ void split(const std::string& str, std::vector<std::string>& cont, char delim);
 
 int main(int argc, char **argv) {
 	// Backup the stdio streambufs
-    // streambuf * cin_streambuf  = cin.rdbuf();
-    // streambuf * cout_streambuf = cout.rdbuf();
-    // streambuf * cerr_streambuf = cerr.rdbuf();
+    streambuf * cin_streambuf  = cin.rdbuf();
+    streambuf * cout_streambuf = cout.rdbuf();
+    streambuf * cerr_streambuf = cerr.rdbuf();
 
-    // FCGX_Request request;
+    FCGX_Request request;
 
-    // FCGX_Init();
-    // FCGX_InitRequest(&request, 0, 0);
+    FCGX_Init();
+    FCGX_InitRequest(&request, 0, 0);
     
     Renderer renderer(800, 600);
 
     Scene scene;
     Camera camera(glm::vec3( 0.0f, 0.0f, 0.0f ), 45.0f);
-    Model ourModel( "res/models/2017.06.28_Final.obj" );
+    Model ourModel( "res/models/happy_recon/happy_final.obj" );
+    // Model ourModel( "res/models/cube/cube.obj" );
+    // Model ourModel( "res/models/head_of_jupiter.obj" );
     scene.add( &ourModel );
 
     float m[] = {1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
@@ -72,47 +74,47 @@ int main(int argc, char **argv) {
     FramebufferReader framebufferReader(3, 800, 600);
 
     // Game loop
-    while ( true ) {        
-    // while (FCGX_Accept_r(&request) == 0) {
-        // fcgi_streambuf cin_fcgi_streambuf(request.in);
-        // fcgi_streambuf cout_fcgi_streambuf(request.out);
-        // fcgi_streambuf cerr_fcgi_streambuf(request.err);
+    // while ( true ) {        
+    while (FCGX_Accept_r(&request) == 0) {
+        fcgi_streambuf cin_fcgi_streambuf(request.in);
+        fcgi_streambuf cout_fcgi_streambuf(request.out);
+        fcgi_streambuf cerr_fcgi_streambuf(request.err);
 
-        // cin.rdbuf(&cin_fcgi_streambuf);
-        // cout.rdbuf(&cout_fcgi_streambuf);
-        // cerr.rdbuf(&cerr_fcgi_streambuf);    
+        cin.rdbuf(&cin_fcgi_streambuf);
+        cout.rdbuf(&cout_fcgi_streambuf);
+        cerr.rdbuf(&cerr_fcgi_streambuf);    
 
-        // const char * uri = FCGX_GetParam("REQUEST_URI", request.envp);
-        // std::string uri_str(uri);
+        const char * uri = FCGX_GetParam("REQUEST_URI", request.envp);
+        std::string uri_str(uri);
 
-        // std::vector<std::string> words;
-        // split(uri_str.substr(10,uri_str.length()), words, ',');
-        // for (unsigned int j=0; j<16; j++) {
-        //     std::cout << (words[j]) << " ";
-        //     m[j] = std::stod(words[j]);
-        // }
-        // camera.fov = std::stod(words[16]);
-        camera.fov = 45.0f;
-        // renderer.useTexture = std::stoi(words[17]);
-        renderer.useTexture = 1;
+        std::vector<std::string> words;
+        split(uri_str.substr(10,uri_str.length()), words, ',');
+        for (unsigned int j=0; j<16; j++) {
+            std::cout << (words[j]) << " ";
+            m[j] = std::stod(words[j]);
+        }
+        camera.fov = std::stod(words[16]);
+        renderer.useTexture = std::stoi(words[17]);
+        // camera.fov = 45.0f;
+        // renderer.useTexture = 1;
 
         ourModel.modelMatrix = glm::make_mat4(m);
 
         renderer.render( &scene, &camera );
 
         // std::cout << "Content-type: text/html\r\n\r\n" << camera.fov;
-        // std::cout << "Content-type: image/png\r\n\r\n";
-        // framebufferReader.writeFrameToCout();
-        // std::cout << std::endl;
-        std::cin.ignore();
+        std::cout << "Content-type: image/png\r\n\r\n";
+        framebufferReader.writeFrameToCout();
+        std::cout << std::endl;
+        // std::cin.ignore();
     }
     
     renderer.close();
     
     // restore stdio streambufs
-    // cin.rdbuf(cin_streambuf);
-    // cout.rdbuf(cout_streambuf);
-    // cerr.rdbuf(cerr_streambuf);
+    cin.rdbuf(cin_streambuf);
+    cout.rdbuf(cout_streambuf);
+    cerr.rdbuf(cerr_streambuf);
 
     return 0;
 }
